d5f70ece0720afb849128ffdf614b1cf3de2d1f9
Justin Ray Angus <angus1@llnl.gov>
2024-07-28 14:44:53 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,7 +8,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import subprocess
 import sys
